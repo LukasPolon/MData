197eb470eb6643ad3863bd62eaeb10cb16a85822
LukasPolon <lukaspolon@gmail.com>
2017-07-16 21:02:20 +0200

    virtualenv and initial project

---
+++ mDataAn_venv/Scripts/activate.bat
@@ -0,0 +1,26 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\LukaszP\Documents\Repos\lp_mgr_main\mDataAn_venv"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+	set "_OLD_VIRTUAL_PROMPT=%PROMPT%"	
+)
+set "PROMPT=(mDataAn_venv) %PROMPT%"
+
+if not defined _OLD_VIRTUAL_PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+)
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
